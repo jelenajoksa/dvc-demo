4854eeee60eb2d5819a489ee7ddd0464de3046c2
jelenajoksa <jelena.joksimovic92@gmail.com>
2022-05-05 15:18:44 +0200

    tox added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'src',
+    version = '0.0.1',
+    description = 'its a wineq package',
+    author= 'jelenajoksa',
+    package = find_packages(),
+    licence = 'MIT'
+)
+
+
